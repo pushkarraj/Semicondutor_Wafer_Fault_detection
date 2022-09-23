8be62728fa1572e26f21919ac20e3bac9a0ce5e3
PUSHKAR RAJ <pushkar.raj6@gmail.com>
2022-09-23 10:23:19 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A machine learing project to detect the faults in semiconductor wafer.',
+    author='Pushkar Raj',
+    license='MIT',
+)
